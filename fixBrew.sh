5af806c6a205cae30d94141b6d0f6e6d248e2bc0
Rareș Chioreanu <rares.davidchioreanu@gmail.com>
2017-09-11 13:17:30 +0300

    Update fixBrew.sh
    Added clear comments and the brew update command.

--- fixBrew.sh
+++ fixBrew.sh
@@ -1,6 +1,7 @@
-echo "Updating brew using the full path"
+echo "--------> Running brew update, because I'm pretty sure that you didn't run it yourself <--------"
+echo "--------> Updating brew using the full path <--------"
 ~/.brew/bin/brew update
-echo "Setting the brew PATH"
+echo "--------> Setting the brew PATH <--------"
 echo "export PATH=~/.brew/bin:$PATH" >> ~/.zshrc
-echo "Done. Please restart $TERM_PROGRAM to apply settings. 😄"
+echo "--------> Done. Please restart $TERM_PROGRAM to apply settings. 😄 <--------"
 exit
