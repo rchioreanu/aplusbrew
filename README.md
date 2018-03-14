99bd004ff0a9e3846c2349430f926e7e2ed75167
Rareș Chioreanu <rares.davidchioreanu@gmail.com>
2018-03-14 15:25:05 +0200

    Update to support proxy

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-I created this script in order to let all the ACADEMY+PLUS students fix brew easily.
+I created this script to let all the ACADEMY+PLUS students fix brew easily.
 
 Thanks @bbardi for all your help!
 
 ## In order to run it:
 
 #### 1. Check that you have a .brew folder in your home directory. If you don't, `brew update`
-#### 2. `curl https://raw.githubusercontent.com/rchioreanu/aplusbrew/master/fixBrew.sh | zsh`
+#### 2. `curl https://raw.githubusercontent.com/rchioreanu/aplusbrew/master/fixBrew.sh | sh`
 #### 3. Done. Enjoy the freedom!
--- fixBrew.sh
+++ fixBrew.sh
@@ -1,7 +1,21 @@
-echo "--------> Running brew update, because I'm pretty sure that you didn't run it yourself <--------"
-echo "--------> Updating brew using the full path <--------"
-~/.brew/bin/brew update
-echo "--------> Setting the brew PATH <--------"
-echo "export PATH=~/.brew/bin:$PATH" >> ~/.zshrc
-echo "--------> Done. Please restart $TERM_PROGRAM to apply settings. 😄 <--------"
-exit
+#!/bin/bash
+
+function repair_brew {
+    echo "--------> Running brew update<--------"
+    /usr/local/bin/brew update
+    echo "--------> Updating brew using the full path <--------"
+    ~/.brew/bin/brew update
+    echo "--------> Setting the brew PATH <--------"
+    echo "export PATH=~/.brew/bin:$PATH" >> ~/.zshrc
+    echo "--------> Done. Please restart $TERM_PROGRAM to apply settings. 😄 <--------"
+}
+
+if [ "git config --list | grep http" ]
+then
+    repair_brew
+else
+    git config --global http.https://github.com.proxy https://121.129.127.209:80
+    repair_brew
+fi
+
+exit;
